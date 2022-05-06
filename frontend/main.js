b0ecd5d0205827bdb14a60237aa41bf284fb95e4
BadmusAnu <badmusanuoluwa@yahoo.com>
2022-05-06 12:06:45 +0100

    Updated index.html page

--- frontend/main.js
+++ frontend/main.js
@@ -8,11 +8,11 @@ const getVisitCount = () => {
     let count = 30;
     fetch(funtionApi).then(response => {
         return response.json()
-    }).then(reponse => {
+    }).then(reponse =>{
         console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;   
-    }).catch(function(error{
+    }).catch(function(error){
         console.log(error);
     });
     return count;
